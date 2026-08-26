cc9ea1ab40858d97e7eaf74ba68f29c398099aab
Yulia Erina <erina.yulya@inbox.ru>
2026-08-26 11:44:00 +0300

    Правка во внешнем виде и инициализации Qv

--- solvePressureFlow.m
+++ solvePressureFlow.m
@@ -1,12 +1,16 @@
-% Основная функция, рассчитывающая давления и расходы в сети
-% Собирается система уравнений вида:
+%% Решение капиллярной сети
+% 
+% Основная функция, рассчитывающая давления и расходы в капиллярной сети.
+% Собирается и итерационно решается нелинейная система уравнений вида:
 % (1) сумма расходов в узле = 0
-% (2) зависимость пвсхода от давления на каждом капилляре
+% (2) зависимость расхода от давления на каждом капилляре (от режима)
 % (3) граничные условия
-% Используется собственный метод Ньютона в calcResidualJacobian()
-% Итог: реальные абсолютные значения давления в узлах и расходов по ребрам
+% Используется собственный метод Ньютона с уменьшением шага для уменьшения
+% невязки и достижения заданной точности.
+
 function Net = solvePressureFlow(Net)
     
+    % Подготовка массивов
     Ny = Net.Ny;
     Nx = Net.Nx;
     
@@ -17,131 +21,81 @@ function Net = solvePressureFlow(Net)
     n = nP+nQH+nQV;
     
     idxP = reshape(1:nP,Ny,Nx-1);
-    
     idxQH = nP + reshape(1:nQH,Ny,Nx);
-    
     idxQV = nP+nQH + reshape(1:nQV,Ny-1,Nx-1);
     
     X = zeros(n,1);
     
+    % Начальное приближение для P:
+    % из предыдущего шага или из граничного условия
     if isfield(Net,'P') && ...
             isequal(size(Net.P),[Ny,Nx+1])
-    
         P0 = Net.P(:,2:Nx);
-    
         X(idxP(:)) = P0(:);
-    
     else
         for i = 1:Ny
             for j = 2:Nx
-    
                 X(idxP(i,j-1)) = ...
                     Net.P0*(Nx+1-j)/Nx;
-    
             end
         end
-    
     end
     
+    % Начальное приближение для Qh:
+    % из предыдущего шага или рассчитывается по режиму
     if isfield(Net,'Qh') && ...
     isequal(size(Net.Qh),[Ny,Nx])
-    
         X(idxQH(:)) = Net.Qh(:);
-    
     else
         % Начальное приближение Q для капилляров с мениском
         for i = 1:Ny
             for j = 1:Nx
-    
-                if Net.StateH(i,j) ~= 1
-                    continue
+                if Net.StateH(i,j) == 1
+                    X(idxQH(:)) = 1e-15; % ненулевая малая величина
                 end
-    
-                r = sqrt(Net.Ah(i,j)/pi);
-    
-                if j == 1
-                    pLeft = Net.P0;
-                else
-                    pLeft = X(idxP(i,j-1));
-                end
-    
-                if j == Nx
-                    pRight = 0;
-                else
-                    pRight = X(idxP(i,j));
-                end
-    
-                dp = pLeft-pRight;
-    
-                Pc = 2*Net.sigma*cos(Net.theta)/r;
-                dpStar = dp-Pc;
-    
-                if Net.theta < pi/2
-                    muSm = Net.mu1;
-                else
-                    muSm = Net.mu2;
-                end
-    
-                B = 2*Net.kdyn*Net.sigma*sin(Net.theta)/r ...
-                    *(muSm/Net.sigma)^(1/3);
-    
-                c = pi*r^2;
-    
-                if dpStar > 0
-                    X(idxQH(i,j)) = c*(dpStar/B)^3;
-                else
-                    X(idxQH(i,j)) = 0;
-                end
-    
             end
         end
-    
     end
     
+    % Начальное приближение для Qv:
     if isfield(Net,'Qv') && ...
             isequal(size(Net.Qv),[Ny-1,Nx-1])
-    
         X(idxQV(:)) = Net.Qv(:);
-    
     end
     
-    % Основной расчет
+    %%-------------------------------------------------------
+    %% Основной расчет
+    %%-------------------------------------------------------
 
     tol = 1e-10;
     maxIter = 50;
     
+    % F - невязка текущего решения
+    % J - якобиан
+    % dx - шаг Ньютона
+
     for iter = 1:maxIter
-    
         [F,J] = calcResidualJacobian(...
             Net,X,idxP,idxQH,idxQV);
-    
         err = norm(F,inf);
-    
         if err < tol
             break
         end
-    
-        dx = J\(-F);
-    
+
+        dx = J\(-F); % стандартный шаг Ньютона
         if any(~isfinite(dx))
             error('Newton: получен некорректный шаг.')
         end
-    
+
         alpha = 1;
-    
         while alpha > 1e-6
-    
-            Xtrial = X + alpha*dx;
-    
+            Xtrial = X + alpha*dx; % пробуем полный шаг
             Ftrial = calcResidual(...
                 Net,Xtrial,idxP,idxQH,idxQV);
-    
             if norm(Ftrial,inf) < err
                 break
             end
-    
-            alpha = alpha/2;
-    
+            alpha = alpha/2; % уменьшаем шаг, если предыдущий не помог
         end
     
         if alpha <= 1e-6
@@ -149,20 +103,19 @@ function Net = solvePressureFlow(Net)
         end
     
         X = Xtrial;
-    
     end
     
     if norm(F,inf) >= tol
         warning('Newton: не достигнута заданная точность.')
     end
 
-    % Подготовка решений
+    %%-------------------------------------------------------
+    %% Подготовка результатов
+    %%-------------------------------------------------------
     
     Net.P = zeros(Ny,Nx+1);
-    
     Net.P(:,1) = Net.P0;
     Net.P(:,Nx+1) = 0;
-    
     Net.P(:,2:Nx) = ...
         reshape(X(idxP(:)),Ny,Nx-1);
     
@@ -173,5 +126,4 @@ function Net = solvePressureFlow(Net)
         reshape(X(idxQV(:)),Ny-1,Nx-1);
     
     Net.NewtonIterations = iter;
-    
 end
