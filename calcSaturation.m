450ec304758e57871ffc509a857c7a17afbf2881
Yulia Erina <erina.yulya@inbox.ru>
2026-09-07 22:02:34 +0300

    Убрала дублирование в Saturation

--- calcSaturation.m
+++ calcSaturation.m
@@ -8,34 +8,20 @@
 % Sat = Sat + dt*v/L
 
 function Net = calcSaturation(Net)
+    Net.H = calcSaturationEval(Net.H, Net.dt, Net.L);
+    Net.V = calcSaturationEval(Net.V, Net.dt, Net.L);
+end
 
-    %%-------------------------------------------------------
-    %% Горизонтальные капилляры
-    %%-------------------------------------------------------
-    
-    for i = 1:size(Net.StateH,1)
-        for j = 1:size(Net.StateH,2)
-            if Net.StateH(i,j)==1 && Net.MoveH(i,j)==1
-                v = abs(Net.Qh(i,j))/Net.Ah(i,j);
-                Net.SatH(i,j) = min(...
-                    1,...
-                    Net.SatH(i,j)+v*Net.dt/Net.L...
-                );
-            end
-        end
-    end
 
-    %%-------------------------------------------------------
-    %% Вертикальные капилляры
-    %%-------------------------------------------------------
-    
-    for i = 1:size(Net.StateV,1)
-        for j = 1:size(Net.StateV,2)
-            if Net.StateV(i,j)==1 && Net.MoveV(i,j)==1
-                v = abs(Net.Qv(i,j))/Net.Av(i,j);
-                Net.SatV(i,j) = min(...
-                    1,...
-                    Net.SatV(i,j)+v*Net.dt/Net.L);
+function Net = calcSaturationEval(Net, dt, L)
+    [Ny, Nx] = size(Net.A);
+    for i = 1:Ny
+        for j = 1:Nx
+            if Net.State(i,j) == 1 && Net.Move(i,j) == 1
+                v = abs(Net.Q(i,j)) / Net.A(i,j);
+                Net.Sat(i,j) = min(...
+                    1, ...
+                    Net.Sat(i,j) + v * dt / L);
             end
         end
     end
