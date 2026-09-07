fe8154baad886bd61a09fe6d886c0a66c8e7cfc6
Yulia Erina <erina.yulya@inbox.ru>
2026-09-07 22:01:45 +0300

    Правки в State

--- calcState.m
+++ calcState.m
@@ -14,17 +14,17 @@ function Net = calcState(Net)
     %% Горизонтальные капилляры
     %%-------------------------------------------------------
     
-    for i = 1:size(Net.StateH,1)
-        for j = 1:size(Net.StateH,2)
-            if Net.StateH(i,j)==1 && Net.SatH(i,j)>=1-tol
-                Net.StateH(i,j)=2;
-                Net.SatH(i,j)=1;
-                if Net.Qh(i,j)>=0
-                    nodeRow=i;
+    for i = 1:Net.H.Nx
+        for j = 1:Net.H.Ny
+            if Net.H.State(i,j)==1 && Net.H.Sat(i,j)>=1-tol
+                Net.H.State(i,j)=2;
+                Net.H.Sat(i,j)=1;
+                
+                nodeRow=i;
+                if Net.H.Q(i,j)>=0
                     nodeCol=j+1;
                     from='L'; % from left side
                 else
-                    nodeRow=i;
                     nodeCol=j;
                     from='R'; % from right side
                 end
@@ -37,14 +37,14 @@ function Net = calcState(Net)
     %% Вертикальные капилляры
     %%-------------------------------------------------------
     
-    for i=1:size(Net.StateV,1)
-        for j=1:size(Net.StateV,2)
-            if Net.StateV(i,j)==1 && Net.SatV(i,j)>=1-tol
-                Net.StateV(i,j)=2;
-                Net.SatV(i,j)=1;
-                if Net.Qv(i,j)>=0
+    for i=1:Net.V.Nx
+        for j=1:Net.V.Ny
+            if Net.V.State(i,j)==1 && Net.V.Sat(i,j)>=1-tol
+                Net.V.State(i,j)=2;
+                Net.V.Sat(i,j)=1;
+                if Net.V.Q(i,j)>=0
                     nodeRow=i+1;
-                    nodeCol=j+1;
+                    nodeCol=j;
                     from='U'; % from upper
                 else
                     nodeRow=i;
@@ -55,7 +55,6 @@ function Net = calcState(Net)
             end
         end
     end
-
 end
 
 
@@ -69,67 +68,43 @@ end
 
 function Net = processNode(Net,row,col,from)
 
-    %%-------------------------------------------------------
-    %% Горизонтальный вправо
-    %%-------------------------------------------------------
-    
-    if col<=Net.Nx
-        if from~='R'
-            if Net.StateH(row,col)==0
-                if Net.Qh(row,col)>0
-                    Net.StateH(row,col)=1;
-                    Net.SatH(row,col)=0; % сатурация обнуляется для гарантии
-                end
-            end
+    %-------------------------------------------------------
+    % Горизонтальный вправо
+    %-------------------------------------------------------
+    if col <= Net.H.Ny && from ~= 'R'
+        if Net.H.State(row,col) == 0 && Net.H.Q(row,col) > 0
+            Net.H.State(row,col) = 1;
+            Net.H.Sat(row,col) = 0;
         end
     end
-    
-    %%-------------------------------------------------------
-    %% Горизонтальный влево
-    %%-------------------------------------------------------
-    
-    if col>1
-        if from~='L'
-            if Net.StateH(row,col-1)==0
-                if Net.Qh(row,col-1)<0
-                    Net.StateH(row,col-1)=1;
-                    Net.SatH(row,col-1)=0;
-                end
-            end
+
+    %-------------------------------------------------------
+    % Горизонтальный влево
+    %-------------------------------------------------------
+    if col > 1 && from ~= 'L'
+        if Net.H.State(row,col-1) == 0 && Net.H.Q(row,col-1) < 0
+            Net.H.State(row,col-1) = 1;
+            Net.H.Sat(row,col-1) = 0;
         end
     end
-    
-    %%-------------------------------------------------------
-    %% Вертикальный вниз
-    %%-------------------------------------------------------
-    
-    if row<Net.Ny
-        if col>1 && col<=Net.Nx
-            if from~='D'
-                if Net.StateV(row,col-1)==0
-                    if Net.Qv(row,col-1)>0
-                        Net.StateV(row,col-1)=1;
-                        Net.SatV(row,col-1)=0;
-                    end
-                end
-            end
+
+    %-------------------------------------------------------
+    % Вертикальный вниз
+    %-------------------------------------------------------
+    if row <= Net.V.Nx && from ~= 'D'
+        if Net.V.State(row,col) == 0 && Net.V.Q(row,col) > 0
+            Net.V.State(row,col) = 1;
+            Net.V.Sat(row,col) = 0;
         end
     end
-    
-    %%-------------------------------------------------------
-    %% Вертикальный вверх
-    %%-------------------------------------------------------
-    
-    if row>1
-        if col>1 && col<=Net.Nx
-            if from~='U'
-                if Net.StateV(row-1,col-1)==0
-                    if Net.Qv(row-1,col-1)<0
-                        Net.StateV(row-1,col-1)=1;
-                        Net.SatV(row-1,col-1)=0;
-                    end
-                end
-            end
+
+    %-------------------------------------------------------
+    % Вертикальный вверх
+    %-------------------------------------------------------
+    if row > 1 && from ~= 'U'
+        if Net.V.State(row-1,col) == 0 && Net.V.Q(row-1,col) < 0
+            Net.V.State(row-1,col) = 1;
+            Net.V.Sat(row-1,col) = 0;
         end
     end
 end
