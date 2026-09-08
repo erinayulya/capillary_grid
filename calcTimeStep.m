bd01735b4f7a926766fa58cc9604c42713dd84ff
Yulia Erina <erina.yulya@inbox.ru>
2026-09-08 10:52:09 +0300

    Правки 1-3 и удаление дублирования для TimeStep

--- calcTimeStep.m
+++ calcTimeStep.m
@@ -7,43 +7,28 @@
 
 function Net = calcTimeStep(Net)
 
+    tminH = calcTimeStepEval(Net.H, Net);
+    tminV = calcTimeStepEval(Net.V, Net);
+    
+    Net.dt = min(tminH, tminV);
+end
+
+
+function tmin = calcTimeStepEval(Cap, Net)
+
     tmin = Inf;
+    for i = 1:Cap.Ny
+        for j = 1:Cap.Nx
+            if Cap.State(i,j)==1 && Cap.Move(i,j)==1
+                A = Cap.A(i,j);
+                S = Cap.Sat(i,j);
     
-    %%-------------------------------------------------------
-    %% Горизонтальные капилляры
-    %%-------------------------------------------------------
-    
-    for i = 1:size(Net.StateH,1)
-        for j = 1:size(Net.StateH,2)
-            if Net.StateH(i,j)==1 && Net.MoveH(i,j)==1
-                A = Net.Ah(i,j);
-                S = Net.SatH(i,j);
                 if S < 1-1e-12
-                    v = abs(Net.Qh(i,j))/A;
+                    v = abs(Cap.Q(i,j))/A;
                     t = (1-S)*Net.L/v;
                     tmin = min(tmin,t);
                 end
             end
         end
     end
-
-    %%-------------------------------------------------------
-    %% Вертикальные капилляры
-    %%-------------------------------------------------------
-    
-    for i = 1:size(Net.StateV,1)
-        for j = 1:size(Net.StateV,2)
-            if Net.StateV(i,j)==1 && Net.MoveV(i,j)==1
-                A = Net.Av(i,j);
-                S = Net.SatV(i,j);
-                if S < 1-1e-12
-                    v = abs(Net.Qv(i,j))/A;
-                    t = (1-S)*Net.L/v;
-                    tmin = min(tmin,t);
-                end
-            end
-        end
-    end
-
-    Net.dt = tmin;
 end
