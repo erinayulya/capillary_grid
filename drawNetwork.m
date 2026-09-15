626c67c40e55612286fea3f7fc25e69c9392456f
Yulia Erina <erina.yulya@inbox.ru>
2026-09-15 12:25:36 +0300

    правки в отрисовке drawNetwork

--- drawNetwork.m
+++ drawNetwork.m
@@ -5,8 +5,8 @@ clf
 hold on
 axis equal
 
-Nx = Net.Nx;
-Ny = Net.Ny;
+Nx = Net.H.Nx;
+Ny = Net.H.Ny;
 
 c0 = [0 0.4470 0.7410];   % первая фаза
 c2 = [0 0 0];             % вторая фаза (черный)
@@ -15,16 +15,16 @@ c2 = [0 0 0];             % вторая фаза (черный)
 %% Горизонтальные
 %%---------------------------------------------------
 
-for i = 1:Ny
+for i = 1:Net.H.Ny
 
     y = Ny-i;
 
-    for j = 1:Nx
+    for j = 1:Net.H.Nx
 
         x1 = j-1;
         x2 = j;
 
-        switch Net.StateH(i,j)
+        switch Net.H.State(i,j)
 
             case 0
 
@@ -40,9 +40,9 @@ for i = 1:Ny
 
             case 1
 
-                if Net.Qh(i,j)>=0
+                if j == 1 || Net.H.Q(i,j) >= 0
 
-                    xm=x1+Net.SatH(i,j);
+                    xm = x1 + Net.H.Sat(i,j);
 
                     plot([x1 xm],[y y],...
                         'Color',c2,...
@@ -54,7 +54,7 @@ for i = 1:Ny
 
                 else
 
-                    xm=x2-Net.SatH(i,j);
+                    xm = x2 - Net.H.Sat(i,j);
 
                     plot([x1 xm],[y y],...
                         'Color',c0,...
@@ -72,13 +72,15 @@ for i = 1:Ny
                     'MarkerSize',8);
 
         end
-        if Net.StateH(i,j) == 1 && Net.MoveH(i,j) == 1
+
+        if Net.H.Regime(i,j) ~= 0
             text((x1+x2)/2, y+0.12, ...
-                sprintf('%d',Net.RegimeH(i,j)), ...
+                sprintf('%d',Net.H.Regime(i,j)), ...
                 'HorizontalAlignment','center', ...
                 'FontSize',9);
         end
-     end
+
+    end
 
 end
 
@@ -86,16 +88,16 @@ end
 %% Вертикальные
 %%---------------------------------------------------
 
-for i = 1:Ny-1
+for i = 1:Net.V.Ny
 
-    for j = 1:Nx-1
+    for j = 1:Net.V.Nx
 
-        x=j;
+        x = j;
 
-        y1=Ny-i;
-        y2=Ny-i-1;
+        y1 = Ny-i+1;
+        y2 = Ny-i;
 
-        switch Net.StateV(i,j)
+        switch Net.V.State(i,j)
 
             case 0
 
@@ -111,9 +113,9 @@ for i = 1:Ny-1
 
             case 1
 
-                if Net.Qv(i,j)>=0
+                if Net.V.Q(i,j) >= 0
 
-                    ym=y1-Net.SatV(i,j);
+                    ym = y1 - Net.V.Sat(i,j);
 
                     plot([x x],[y1 ym],...
                         'Color',c2,...
@@ -125,7 +127,7 @@ for i = 1:Ny-1
 
                 else
 
-                    ym=y2+Net.SatV(i,j);
+                    ym = y2 + Net.V.Sat(i,j);
 
                     plot([x x],[y1 ym],...
                         'Color',c0,...
@@ -143,9 +145,10 @@ for i = 1:Ny-1
                     'MarkerSize',8);
 
         end
-        if Net.StateV(i,j) == 1 && Net.MoveV(i,j) == 1
+
+        if Net.V.Regime(i,j) ~= 0
             text(x+0.12, (y1+y2)/2, ...
-                sprintf('%d',Net.RegimeV(i,j)), ...
+                sprintf('%d',Net.V.Regime(i,j)), ...
                 'HorizontalAlignment','left', ...
                 'VerticalAlignment','middle', ...
                 'FontSize',9);
@@ -159,15 +162,11 @@ end
 %% Узлы
 %%---------------------------------------------------
 
-%%---------------------------------------------------
-% Новое положение менисков
-%%---------------------------------------------------
+for i = 1:Ny
 
-for i=1:Ny
+    y = Ny-i;
 
-    y=Ny-i;
-
-    for j=0:Nx
+    for j = 1:Nx-1
 
         plot(j,y,...
             'ko',...
@@ -179,74 +178,87 @@ for i=1:Ny
 end
 
 %%---------------------------------------------------
-% Предыдущее положение менисков
+%% Предыдущее положение менисков
 %%---------------------------------------------------
 
-if isfield(Net,'SatH_prev')
+if isfield(Net.H,'Sat_prev')
 
-%% Горизонтальные
-for i = 1:Ny
-    y = Ny-i;
+    %% Горизонтальные
 
-    for j = 1:Nx
+    for i = 1:Net.H.Ny
 
-        if Net.StateH_prev(i,j) ~= 1
-            continue
+        y = Ny-i;
+
+        for j = 1:Net.H.Nx
+
+            if Net.H.State_prev(i,j) ~= 1
+                continue
+            end
+
+            x1 = j-1;
+            x2 = j;
+
+            if j == 1 || Net.H.Q(i,j) >= 0
+                xm = x1 + Net.H.Sat_prev(i,j);
+            else
+                xm = x2 - Net.H.Sat_prev(i,j);
+            end
+
+            plot(xm,y,...
+                'o',...
+                'Color',[0.5 0.5 0.5],...
+                'MarkerFaceColor',[0.5 0.5 0.5],...
+                'MarkerSize',6);
+
         end
-
-        x1 = j-1;
-        x2 = j;
-
-        if Net.Qh(i,j) >= 0
-            xm = x1 + Net.SatH_prev(i,j);
-        else
-            xm = x2 - Net.SatH_prev(i,j);
-        end
-
-        plot(xm,y,...
-            'o',...
-            'Color',[0.5 0.5 0.5],...
-            'MarkerFaceColor',[0.5 0.5 0.5],...
-            'MarkerSize',6);
 
     end
-end
 
+    %% Вертикальные
 
-%% Вертикальные
-for i = 1:Ny-1
-    for j = 1:Nx-1
+    for i = 1:Net.V.Ny
 
-        if Net.StateV_prev(i,j) ~= 1
-            continue
+        for j = 1:Net.V.Nx
+
+            if Net.V.State_prev(i,j) ~= 1
+                continue
+            end
+
+            x = j;
+
+            y1 = Ny-i+1;
+            y2 = Ny-i;
+
+            if Net.V.Q(i,j) >= 0
+                ym = y1 - Net.V.Sat_prev(i,j);
+            else
+                ym = y2 + Net.V.Sat_prev(i,j);
+            end
+
+            plot(x,ym,...
+                'o',...
+                'Color',[0.5 0.5 0.5],...
+                'MarkerFaceColor',[0.5 0.5 0.5],...
+                'MarkerSize',6);
+
         end
-
-        x = j;
-
-        y1 = Ny-i;
-        y2 = Ny-i-1;
-
-        if Net.Qv(i,j) >= 0
-            ym = y1 - Net.SatV_prev(i,j);
-        else
-            ym = y2 + Net.SatV_prev(i,j);
-        end
-
-        plot(x,ym,...
-            'o',...
-            'Color',[0.5 0.5 0.5],...
-            'MarkerFaceColor',[0.5 0.5 0.5],...
-            'MarkerSize',6);
 
     end
-end
 
 end
 
-xlim([-0.2 Nx+0.2])
-ylim([-0.2 Ny-1+0.2])
 
 axis equal
+
+xl = xlim;
+yl = ylim;
+
+dx = diff(xl);
+dy = diff(yl);
+
+xlim(xl + [-0.05 0.05]*dx)
+ylim(yl + [-0.05 0.05]*dy)
+
 box on
 
 xlabel('x')
